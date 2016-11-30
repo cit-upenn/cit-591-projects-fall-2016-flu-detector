6c6d524a474178f0f8fb66fd6b91c5aacfe3f8e6
OrzAngel <junche@seas.upenn.edu>
2016-11-30 17:23:07 -0500

    Collector updated

---
+++ src/Collector.java
@@ -0,0 +1,103 @@
+
+import twitter4j.GeoLocation;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class Collector {
+	
+	private Twitter twitter;
+	private String startTime;
+	private String endTime;
+	private GeoLocation location;
+	
+	public Collector() {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("VzKbJjQHrc3fxS7hm2DGGi3hm")
+		  .setOAuthConsumerSecret("sB6lvUpN601KWXk0tiE6VgfEu6oVtoToNKrN46Ir3D82gF7IEU")
+		  .setOAuthAccessToken("1036064185-bacpuZqOca8iSdUF8neqKA5uMok1zDi1vXalGAw")
+		  .setOAuthAccessTokenSecret("VC610RpP6mcLk0L9qB7kcbe6IS0woSsuxxMEOyOhDc8ie");
+
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("JPlfMmqWtSqJzmENJV7FjbbyZ")
+		  .setOAuthConsumerSecret("1LPzdw2vuQFGHvs1ZiW7GpcOzOBLRhYuQkvBVOhKBHU5EUXGRT")
+		  .setOAuthAccessToken("512682400-xWHuVGLbkIWeEJY0OVyG5ATUOrDq0DYIm59nqjTz")
+		  .setOAuthAccessTokenSecret("FyKmeCiG5aQHSSd0GrlAxi6kpOwjXn4ab3f2ttHWrWI8l");
+		
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		
+		
+		twitter = tf.getInstance();
+
+
+	}
+	
+	public void setSince(String startTime) {
+		this.startTime = startTime;
+	}
+	
+	public void setUntil(String endTime){
+		this.endTime = endTime;
+	}
+	
+	public void setLocation(double lat, double lon) {
+		location = new GeoLocation(lat,lon);
+		
+	}
+	
+	public int search(String keyword) throws TwitterException {
+		
+		Query query = new Query(keyword);
+		query.setCount(100);
+		query.setSince(startTime);
+		query.setUntil(endTime);
+		query.setGeoCode(location, 150, Query.MILES);
+		
+		QueryResult result;
+		int count = 0;
+
+		for(;;) {
+			
+			result = twitter.search(query);
+			count += result.getTweets().size();
+			
+//			Status status = result.getTweets().get(0);
+//			System.out.println(status.getUser().getScreenName() + status.getCreatedAt()+ ":" + status.getText() );
+			
+			if (!result.hasNext()){
+				return count;
+			}
+			
+			query = result.nextQuery();
+//			
+		}
+		
+	}
+	/*
+	 * https://dev.twitter.com/rest/reference/get/search/tweets
+	 * the search index has a 7-day limit. In other words, no tweets will be found for a date older than one week.
+	 */
+	// some commit from another local repo
+	//test pull cofiliction
+	
+	public static void main(String[] args) throws TwitterException {
+		
+		String[] keywords = {"flu symptoms","influenza","fever flu","fever cough","cold flu"};
+		int[] counts = new int[keywords.length];
+		
+		Collector c = new Collector();
+		c.setSince("2016-11-27");
+		c.setUntil("2016-11-28");
+		c.setLocation(39.954435, -75.194937);
+		
+		for (int i = 0 ; i < keywords.length ; i ++) {
+			counts[i] = c.search(keywords[i]);
+			System.out.printf("The number of tweets containing \"%s\" : %d%n",keywords[i],counts[i]);
+		}
+		
+	}
+}
