570bc3567fe07e2d0821aa91f7bcb18bcd0b969f
OrzAngel <junche@seas.upenn.edu>
2016-12-04 21:29:27 -0500

    Collector updated by catching the TwitterExcepti

--- src/Collector.java
+++ src/Collector.java
@@ -4,8 +4,6 @@ import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 public class Collector {
 	
@@ -15,9 +13,7 @@ public class Collector {
 	private GeoLocation location;
 	
 	public Collector() {
-
 		twitter = TwitterConnector.getTwitter();
-
 	}
 	
 	public void setSince(String startTime) {
@@ -33,55 +29,39 @@ public class Collector {
 		
 	}
 	
-	public int search(String keyword) throws TwitterException {
+	public int search(String keyword) throws LocationNotSelectedExeption {
+		
+		if (location == null) {
+			throw new LocationNotSelectedExeption();
+		}
 		
 		Query query = new Query(keyword);
+		query.setGeoCode(location, 150, Query.MILES);
 		query.setCount(100);
 		query.setSince(startTime);
 		query.setUntil(endTime);
-		if (location == null) {
-			throw new LocationNotSelectedExeption()
-		}
-		query.setGeoCode(location, 150, Query.MILES);
 		
 		QueryResult result;
 		int count = 0;
 
 		for(;;) {
 			
-			result = twitter.search(query);
-			count += result.getTweets().size();
-					
-			if (!result.hasNext()){
-				return count;
+			try {
+				
+				result = twitter.search(query);
+				count += result.getTweets().size();
+				
+				if (!result.hasNext()){
+					return count;
+				}				
+		
+				query = result.nextQuery();
+		
+			} catch (TwitterException e) {
+				twitter = TwitterConnector.getTwitterBackup();
 			}
 			
-			query = result.nextQuery();
-//			
 		}
-		
 	}
-	/*
-	 * https://dev.twitter.com/rest/reference/get/search/tweets
-	 * the search index has a 7-day limit. In other words, no tweets will be found for a date older than one week.
-	 */
-	// some commit from another local repo
-	//test pull cofiliction
-	
-	public static void main(String[] args) throws TwitterException {
-		
-		String[] keywords = {"flu symptoms","influenza","fever flu","fever cough","cold flu"};
-		int[] counts = new int[keywords.length];
-		
-		Collector c = new Collector();
-		c.setSince("2016-11-27");
-		c.setUntil("2016-11-28");
-		c.setLocation(new GeoLocation(39.954435, -75.194937));
-		
-		for (int i = 0 ; i < keywords.length ; i ++) {
-			counts[i] = c.search(keywords[i]);
-			System.out.printf("The number of tweets containing \"%s\" : %d%n",keywords[i],counts[i]);
-		}
-		
-	}
+
 }
