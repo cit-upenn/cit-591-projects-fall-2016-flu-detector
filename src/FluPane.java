566aa9370ea9b4cf293214ad3b7cba66a4c4ef52
Luyi Yang <LuyiYang@hou0914.wireless-pennnet.upenn.edu>
2016-12-07 15:32:28 -0500

    Integrated FluPane with the new design

--- src/FluPane.java
+++ src/FluPane.java
@@ -8,6 +8,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import twitter4j.GeoLocation;
@@ -62,8 +65,7 @@ public class FluPane extends Application{
 			try {
 				isFlu = f.alert();
 			} catch (LocationNotSelectedExeption e1) {
-				// TODO Auto-generated catch block
-//				e1.printStackTrace();
+				
 				AlertBox.display("Flu Results", "Please select your location first.");
 				return;
 			}
@@ -84,18 +86,53 @@ public class FluPane extends Application{
 
 		BorderPane layout = new BorderPane();
 		BorderPane.setAlignment(button, Pos.CENTER);
-		BorderPane.setMargin(button, new Insets(12, 12, 20, 12));
+		BorderPane.setMargin(button, new Insets(12, 12, 12, 12));
 
 		layout.setBottom(button);
 		
 		BorderPane.setAlignment(locationMenu, Pos.CENTER);
+		BorderPane.setMargin(locationMenu, new Insets(12, 12, 12, 12));
 		layout.setTop(locationMenu);
 
 		layout.setCenter(mg.getMapView());
 		
 		Scene scene = new Scene(layout, 600, 600);
+		
+		Button button1 = new Button("Let's start!");
+		button1.setOnAction(e -> window.setScene(scene));
+		
+		
+		
+		BorderPane layout1 = new BorderPane();
+		
 
-		window.setScene(scene);
+		ImageView imv = new ImageView();
+		String remoteUrl = "http://blog.londondrugs.com/wp-content/uploads/2016/01/cold-flu.jpg";
+//		String remoteUrl = "httkdsgagnarituip://blog.londondrugs.com/wp-content/uploads/2016/01/cold-flue.jpg";
+		
+		try {
+			
+			
+			Image remoteImage = new Image(remoteUrl, true);
+			imv.setImage(remoteImage);
+			
+			
+			layout1.setCenter(imv);
+			
+			layout1.setBottom(button1);
+			BorderPane.setAlignment(button1, Pos.CENTER);
+			BorderPane.setMargin(button1, new Insets(12, 12, 12, 12));
+			
+			Scene scene1 = new Scene(layout1, 600, 400);
+			window.setScene(scene1);
+			
+			
+		} catch (IllegalArgumentException e) {
+			
+			window.setScene(scene);
+			
+		}
+
 		window.show();
 
 
