e145d55b975497e3335e1f290675b48166c972dd
OrzAngel <junche@seas.upenn.edu>
2016-12-12 22:27:07 -0500

    add javadoc for Collector

--- src/Collector.java
+++ src/Collector.java
@@ -1,4 +1,4 @@
-//
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -9,6 +9,19 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
+/**
+ * This class collects data from the twitter API. 
+ * This API is not able provide data more than 7 days old</br>
+ * Creates a twitter instance form the {@link TwitterConnector} class and then 
+ * counts the number of tweets containing some keyword during one time period at a given location 
+ * using the search method from {@link Twitter}. </br>
+ * Since our goal is to counts several different keywords during the same period at the same location, 
+ * I decide not to pass start time, end time and location as parameters of the search method. But store them as 
+ * instance variables and set them use mutator method before the search method being called. </br>
+ * Setting a location before searching is required in order not to reach the twitter API search rate limit.
+ * @author CJC
+ *
+ */
 public class Collector {
 	
 	private Twitter twitter;
@@ -17,30 +30,55 @@ public class Collector {
 	private GeoLocation location;
 	private ArrayList<Status> exampleTweets;
 	private Random rand;
+	private boolean isBackup;
 	
+	/**
+	 * Constructor : initialize a collector;
+	 */
 	public Collector() {
 		twitter = TwitterConnector.getTwitter();
 		exampleTweets = new ArrayList<>();
 		rand = new Random();
+		isBackup = false;
 	}
 	
+	/**
+	 * clear all the exampleTweets for the last period.
+	 */
 	public void resetCollector() {
 		exampleTweets.clear();
 	}
 	
+	/**
+	 * Sets the date the collector starts collecting data. </br>
+	 * Tweets sent on this day are counted.
+	 * @param startTime the start date in the format "YYYY-MM-DD"
+	 */
 	public void setSince(String startTime) {
 		this.startTime = startTime;
 	}
 	
+	/**
+	 * Sets the date the collector stops collecting data. </br>
+	 * Tweets sent on this day are NOT counted.
+	 * @param endTime the end date in the format "YYYY-MM-DD"
+	 */
 	public void setUntil(String endTime){
 		this.endTime = endTime;
 	}
 	
+	/**
+	 * Set the location the collector searches for tweets
+	 * @param location the location
+	 */
 	public void setLocation(GeoLocation location) {
 		this.location = location;
-		
 	}
 	
+	/**
+	 * get and remove one example tweet from the example tweets list.
+	 * @return one tweet {@link Status}
+	 */
 	public Status getExampleTweet() {
 		int bound = exampleTweets.size();
 		if (bound <= 0) {
@@ -50,6 +88,15 @@ public class Collector {
 		return exampleTweets.remove(index);
 	}
 	
+	/**
+	 * Counts the number of tweet during the time period at given location</br>
+	 * Creates a Query object based on the start time, end time and location set before.</br>
+	 * Calls search method on twitter object to search for the query and updates the query to next page until all the tweets have been counted.</br> 
+	 * Stores all the tweets in a list for further use 
+	 * @param keyword the keyword to be counted
+	 * @return the keyword count
+	 * @throws LocationNotSelectedExeption if the location is not specified
+	 */
 	public int search(String keyword) throws LocationNotSelectedExeption {
 		
 		if (location == null) {
@@ -57,11 +104,13 @@ public class Collector {
 		}
 		
 		Query query = new Query(keyword);
+		//searching for tweets within a circle centering at location with a radius of 150 miles
 		query.setGeoCode(location, 150, Query.MILES);
 		query.setCount(100);
+		//start time included, end time excluded
 		query.setSince(startTime);
 		query.setUntil(endTime);
-		
+	
 		QueryResult result;
 		int count = 0;
 
@@ -78,8 +127,17 @@ public class Collector {
 				}				
 		
 				query = result.nextQuery();
-		
+				
 			} catch (TwitterException e) {
+				/*
+				 * If anything unexpected happened, try the backup twitter instance first.
+				 * If the backup one failed either, terminate the whole program. 
+				 */
+				if (isBackup){
+					System.out.println(e.getMessage());
+					System.exit(0);
+				}
+				isBackup = true;
 				twitter = TwitterConnector.getTwitterBackup();
 			}
 			
